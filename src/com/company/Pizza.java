f89ef84693ecee989f9aa45b886e09aff4b2223e
ParaplanAspen <south_w@ukr.net>
2021-10-25 18:30:53 +0300

    pizza brunch new class

--- src/com/company/Pizza.java
+++ src/com/company/Pizza.java
@@ -1,11 +1,16 @@
 package com.company;
 
 public class Pizza {
-    double basic; // pizza shape
+    Circle basic; // pizza shape
     String composition; // pizza composition
     String name; // pizza name
     double price; // pizza price
-    
-
-
-}
+    double radius;
+    Pizza(Circle bas, String com, String nam, double pr, double rad){
+        basic=bas;
+        composition=com;
+        name=nam;
+        price=pr;
+        radius=rad;
+    }
+   }

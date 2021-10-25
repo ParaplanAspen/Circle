cca2a59fcd960ad638e395b5cef33ea6b9eca109
ParaplanAspen <south_w@ukr.net>
2021-10-25 15:48:30 +0300

    pizza brunch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,20 @@
 package com.company;
-
 /**
  * Определяем объект krug
- *
  */
 public class Main {
 
     public static void main(String[] args) {
-        Circle krug= new Circle(25,"white");
+        Circle krug = new Circle(25, "white");
         double dlin;
         double sq;
         String col;
-        dlin=krug.dlina();
-        sq=krug.square();
-        col= krug.colour;
-        System.out.println("Длина окружности равна"+" "+dlin);
-        System.out.println("Площадь окружности равна"+" "+sq);
-        System.out.println("Цвет окружности равен"+" "+col);
+        dlin = krug.dlina();
+        sq = krug.square();
+        col = krug.colour;
+        System.out.println("Длина окружности равна" + " " + dlin);
+        System.out.println("Площадь окружности равна" + " " + sq);
+        System.out.println("Цвет окружности равен" + " " + col);
 
     }
 
---
+++ src/com/company/Pizza.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Pizza {
+    double basic; // pizza shape
+    String composition; // pizza composition
+    String name; // pizza name
+    double price; // pizza price
+    
+
+
+}
